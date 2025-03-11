e09d856c3b7f0597ff43e1256291d6820f2d2206
sangledev95 <lqsang19952@gmail.com>
2025-03-11 10:06:58 +0700

    Upload 1

--- pages/api/upload-file.ts
+++ pages/api/upload-file.ts
@@ -15,6 +15,7 @@ export default async function handler(
   res: NextApiResponse
 ) {
   try {
+    console.log("req ==== ", req);
     if (req.method !== "POST") {
       return res.status(405).json({ message: "Method Not Allowed" });
     }
